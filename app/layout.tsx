a4c6114aa34d9eb23822673dfa4a805fae6fe215
bittucse <bittucse@outlook.com>
2024-07-22 08:44:17 +0530

    fix rootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,29 +3,41 @@ import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import './globals.css'
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-export const metadata: Metadata={
-  title:'colabeditor',
-  description: 'A collaborative editor',
+export const metadata: Metadata = {
+  title: 'colabeditor',
+  description: 'A collaborative editor application',
 }
 
-export default function RootLayout({ children }: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+            {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
