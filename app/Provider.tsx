7c80af856cfa1fa4ddfdddfde7313b4fce31fbb6
bittucse <bittucse@outlook.com>
2024-07-23 21:45:02 +0530

    add LiveBlocks

---
+++ app/Provider.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import {
+    LiveblocksProvider,
+    RoomProvider,
+    ClientSideSuspense,
+  } from "@liveblocks/react/suspense";
+import { ReactNode } from "react";
+
+const Provider = ({children}:{children:ReactNode}) => {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        {children}
+      </ClientSideSuspense>
+  </LiveblocksProvider>
+  )
+}
+
+export default Provider
