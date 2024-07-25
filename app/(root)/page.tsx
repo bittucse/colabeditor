71850234b857932f208dfbdbb7e71daa3daa5320
bittucse <bittucse@outlook.com>
2024-07-25 11:20:04 +0530

    update HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,40 @@
+import AddDocumentBtn from '@/components/AddDocumentBtn'
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
+
+  const documents=[];
   return (
-    <div>
-      <Button>
-        Click Me
-      </Button>
+    <main className=' home-container'>
+      <Header className='sticky left-0 top-0'>
+    <div className='flex items-center gap-2 lg:gap-4'>
+      Notification
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
+
+      </Header>
+
+      {documents.length>0?(
+        <div>
+
+        </div>
+      ):(
+        <div className='document-list-empty'>
+          <Image src="/assets/icons/doc.svg" alt='document' 
+          width={40}
+          height={40}
+          className='max-auto'/>
+          <AddDocumentBtn/>
+
+        </div>
+      )}
+    </main>
   )
 }
 
---
+++ components/AddDocumentBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddDocumentBtn = () => {
+  return (
+    <div>
+      AddDocument
+    </div>
+  )
+}
+
+export default AddDocumentBtn
