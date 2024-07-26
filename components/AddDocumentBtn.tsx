9bff905045b278e90503223414532dc27fac33d9
bittucse <bittucse@outlook.com>
2024-07-26 21:12:38 +0530

    update HomeScreen

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,10 +1,21 @@
+'use client';
 import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
 
-const AddDocumentBtn = () => {
+const AddDocumentBtn = ({userId,email}: AddDocumentBtnProps) => {
+  const addDocumentHandler =async()=>{
+
+  }
   return (
-    <div>
-      AddDocument
-    </div>
+    <Button className=' gradient-blue flex gap-1 shadow-md' type='submit' onClick={addDocumentHandler}>
+      <Image src='/assets/icons/add.svg' alt='add button'
+      width={24}
+      height={24}/>
+      <p className='hidden sm:block'>
+        Start a blank document
+      </p>
+    </Button>
   )
 }
 
