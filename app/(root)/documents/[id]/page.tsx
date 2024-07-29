016f41c23be86f64f6a302439c5cc97c9d96a0f2
bittucse <bittucse@outlook.com>
2024-07-29 17:29:32 +0530

    add getDocument

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,7 @@
 import CollaborativeRoom from '@/components/CollaborativeRoom'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { getDocument } from '@/lib/actions/room.actions'
 import {
   ClerkProvider,
   SignInButton,
@@ -8,9 +9,23 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Document = () => {
+const Document = async ({params:{ id }}:SearchParamProps) => {
+  const clerkUser= await currentUser();
+  if(!clerkUser) redirect('/sign-in');
+
+  const room=await getDocument({
+    roomId:id,
+    userId:clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if(!room) redirect('/');
+
+  // TODO: assess the permissions
+
   return (
     <main className='flex w-full flex-col items-center'>
       <CollaborativeRoom/>
